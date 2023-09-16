05b4b66951b1245374ac338b2921fb7e16b8b5ec
Harika Tipparaju <nithinkamineni1@gmail.com>
2023-09-16 04:59:46 -0400

    d3-graphs

---
+++ src/views/dashboard/Default/Heatmap.js
@@ -0,0 +1,191 @@
+import { useState } from "react";
+import styles from "./tooltip.module.css";
+import { useMemo } from "react";
+import * as d3 from "d3";
+
+const MARGIN = { top: 10, right: 50, bottom: 30, left: 50 };
+
+const Renderer = ({
+  width,
+  height,
+  setHoveredCell,
+  data
+}) => {
+  const boundsWidth = width - MARGIN.right - MARGIN.left;
+  const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+
+  const allYGroups = useMemo(() => [...new Set(data.map((d) => d.y))], [data]);
+  const allXGroups = useMemo(() => [...new Set(data.map((d) => d.x))], [data]);
+
+  const [min = 0, max = 0] = d3.extent(data.map((d) => d.value));
+
+  const xScale = useMemo(() => {
+    return d3
+      .scaleBand()
+      .range([0, boundsWidth])
+      .domain(allXGroups)
+      .padding(0.01);
+  }, [data, width]);
+
+  const yScale = useMemo(() => {
+    return d3
+      .scaleBand()
+      .range([boundsHeight, 0])
+      .domain(allYGroups)
+      .padding(0.01);
+  }, [data, height]);
+
+  var colorScale = d3
+    .scaleSequential()
+    .interpolator(d3.interpolateInferno)
+    .domain([min, max]);
+
+  const allShapes = data.map((d, i) => {
+    const x = xScale(d.x);
+    const y = yScale(d.y);
+
+    if (d.value === null || !x || !y) {
+      return;
+    }
+
+    return (
+      <rect
+        key={i}
+        r={4}
+        x={xScale(d.x)}
+        y={yScale(d.y)}
+        width={xScale.bandwidth()}
+        height={yScale.bandwidth()}
+        opacity={1}
+        fill={colorScale(d.value)}
+        rx={5}
+        stroke={"white"}
+        onMouseEnter={(e) => {
+          setHoveredCell({
+            xLabel: "group " + d.x,
+            yLabel: "group " + d.y,
+            xPos: x + xScale.bandwidth() + MARGIN.left,
+            yPos: y + xScale.bandwidth() / 2 + MARGIN.top,
+            value: Math.round(d.value * 100) / 100
+          });
+        }}
+        onMouseLeave={() => setHoveredCell(null)}
+        cursor="pointer"
+      />
+    );
+  });
+
+  const xLabels = allXGroups.map((name, i) => {
+    const x = xScale(name);
+
+    if (!x) {
+      return null;
+    }
+
+    return (
+      <text
+        key={i}
+        x={x + xScale.bandwidth() / 2}
+        y={boundsHeight + 10}
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fontSize={10}
+      >
+        {name}
+      </text>
+    );
+  });
+
+  const yLabels = allYGroups.map((name, i) => {
+    const y = yScale(name);
+
+    if (!y) {
+      return null;
+    }
+
+    return (
+      <text
+        key={i}
+        x={-5}
+        y={y + yScale.bandwidth() / 2}
+        textAnchor="end"
+        dominantBaseline="middle"
+        fontSize={10}
+      >
+        {name}
+      </text>
+    );
+  });
+
+  return (
+    <svg width={width} height={height}>
+      <g
+        width={boundsWidth}
+        height={boundsHeight}
+        transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+      >
+        {allShapes}
+        {xLabels}
+        {yLabels}
+      </g>
+    </svg>
+  );
+};
+
+const Tooltip = ({ interactionData, width, height }) => {
+  if (!interactionData) {
+    return null;
+  }
+
+  return (
+    <div
+      style={{
+        width,
+        height,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        pointerEvents: "none"
+      }}
+    >
+      <div
+        className={styles.tooltip}
+        style={{
+          position: "absolute",
+          left: interactionData.xPos,
+          top: interactionData.yPos
+        }}
+      >
+        <TooltipRow label={"x"} value={interactionData.xLabel} />
+        <TooltipRow label={"y"} value={interactionData.yLabel} />
+        <TooltipRow label={"value"} value={String(interactionData.value)} />
+      </div>
+    </div>
+  );
+};
+
+const TooltipRow = ({ label, value }) => (
+  <div>
+    <b>{label}</b>
+    <span>: </span>
+    <span>{value}</span>
+  </div>
+);
+
+const Heatmap = ({ width, height, data }) => {
+  const [hoveredCell, setHoveredCell] = useState(null);
+
+  return (
+    <div style={{ position: "relative" }}>
+      <Renderer
+        width={width}
+        height={height}
+        data={data}
+        setHoveredCell={setHoveredCell}
+      />
+      <Tooltip interactionData={hoveredCell} width={width} height={height} />
+    </div>
+  );
+};
+
+export default Heatmap;
--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 // material-ui
-import { Grid } from '@mui/material';
+// import { Grid } from '@mui/material';
 
 import * as React from 'react';
 import Switch from '@mui/material/Switch';
@@ -9,6 +9,7 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Heatmap from "./Heatmap";
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
@@ -16,6 +17,50 @@ const Dashboard = () => {
   useEffect(() => {
     setLoading(false);
   }, []);
+
+  const nCol = 10;
+const nRow = 5;
+
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+let data = [];
+
+for (let x = 0; x < nCol; x++) {
+  for (let y = 0; y < nRow; y++) {
+    data.push({
+      x: alphabet[x],
+      y: alphabet[y],
+      value: Math.random() * 40
+    });
+  }
+}
 
   return (
     // add here shellhacks
@@ -49,6 +94,8 @@ const Dashboard = () => {
         />
       </FormGroup>
     </FormControl>
+
+    <Heatmap width={700} height={400} data={data} />     
     </>
   );
 };
